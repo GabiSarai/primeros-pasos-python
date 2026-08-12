87254cb7b397ea069ee868f48d62bd5cfe32ebab
Gabriela Laguna <gabrielalaguna555@gmail.com>
2026-08-11 22:54:37 -0500

    Create practica_anidados.py

---
+++ practica_anidados.py
@@ -0,0 +1,11 @@
+x = int(input("ingrese entero: "))
+
+if x > 5: # True
+    if x == 6: # False
+        print("anidado: x == 6")
+    elif x == 10: # True
+        print("anidado: x == 10")
+    else:
+        print("anidado: else")
+else:
+    print("else")
