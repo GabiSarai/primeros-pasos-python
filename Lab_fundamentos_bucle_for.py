581c6156756e7740188a33274d39c96b05247b36
Gabriela Laguna <gabrielalaguna555@gmail.com>
2026-08-18 22:49:51 -0500

    Create Lab_fundamentos_bucle_for.py
    a idea detrás de esto es que agregar la palabra Mississippi a un número al contar los segundos en voz alta hace que suene más cercano al reloj, y por lo tanto "un Mississippi, dos Mississippi, tres Mississippi" tomará aproximadamente unos tres segundos reales de tiempo. A menudo lo usan los niños que juegan al escondite para asegurarse de que el buscador haga un conteo honesto.
    
    La tarea es muy simple aquí: escribe un programa que use un bucle for para "contar de forma mississippi" hasta cinco. Habiendo contado hasta cinco, el programa debería imprimir en la pantalla el mensaje final "¡Listos o no, ahí voy!"

---
+++ Lab_fundamentos_bucle_for.py
@@ -0,0 +1,13 @@
+import time
+
+for contar_m in range(1, 6):
+    print(contar_m, "Mississippi")
+    time.sleep(1)
+    
+print("Lista o no, aqui voy!")
+    
+# Escribe un bucle for que cuente hasta cinco.
+    # Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+    # Cuerpo del bucle, emplea : time.sleep(1)
+
+# Escribe una función print con el mensaje final.
